29610a7fed6ea8e92c0bdc2a0174b269243cf10d
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-04-07 19:38:30 -0700

    add contact search by id and contact delete by id features

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,30 @@ function AddressBook() {
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId(); // gives contact an Id
   this.contacts.push(contact); // adds,contact to contacts array in AddressBook
-,
+}
 
 AddressBook.prototype.assignId = function(contact) {
   this.currentId++;
   return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i].id === id) {
+      return this.contacts[i];
+    }
+  };
+  return false;
+}
+
+AddresssBook.prototype.deleteContact = function(id) {
+  for (var i = o; i < this.contacts.length; i++) {
+    if (this.contacts[i].id === id) {
+      delete this.contacts[i];
+      return true;
+    }
+  };
+  return false;
 }
 
 // Business Logic for Contacts
